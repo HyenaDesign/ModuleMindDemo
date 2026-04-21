de11018caf1b3267b2651f29490d291b13a89afe
HyenaDesign <147380042+HyenaDesign@users.noreply.github.com>
2026-04-21 16:16:43 +0200

    fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,9 @@
-import { Redirect } from "expo-router";
+import { Text, View } from 'react-native';
 
-export default function Index() {
-  return <Redirect href="/Upload" />;
+export default function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>🔥 My real demo is working!</Text>
+    </View>
+  );
 }
